52db011dc8b2749cb58743a66d5170b0fa032a7d
Dina Pal <dinapal.dev@gmail.com>
2022-10-30 18:38:50 +0530

    update eqnueue_scripts.phpp file

--- inc/enqueue_scripts.php
+++ inc/enqueue_scripts.php
@@ -10,8 +10,7 @@ add_action( 'admin_enqueue_scripts', 'css_admin_enqueue_scripts' );
 
 	function css_enqueue_scripts(){
 		wp_enqueue_style('css-front-css', plugin_dir_url(__DIR__).'assets/style.css', null, CSS_VERSION );
-		wp_enqueue_script('css-front-scripts',plugin_dir_url(__DIR__).'assets/scripts.js', array('jquery'), CSS_VERSION,
-			true	);
+		wp_enqueue_script('css-front-scripts',plugin_dir_url(__DIR__).'assets/scripts.js', array('jquery'), CSS_VERSION, true);
 	}
 
 	/**
@@ -23,9 +22,6 @@ add_action( 'admin_enqueue_scripts', 'css_admin_enqueue_scripts' );
 		if ( 'toplevel_page_social-share' != $screen ) {
 			return;
 		}
-		wp_enqueue_script( 'css-admin-scripts', plugin_dir_url( __DIR__ ) . 'assets/admin-scripts.js', array('jquery'),
-		CSS_VERSION,
-			true );
-		wp_enqueue_style('css-admin-css', plugin_dir_url(__DIR__).'assets/admin-style.css', null,
-			CSS_VERSION );
+		wp_enqueue_script( 'css-admin-scripts', plugin_dir_url( __DIR__ ) . 'assets/admin-scripts.js', array('jquery'), CSS_VERSION, true );
+		wp_enqueue_style('css-admin-css', plugin_dir_url(__DIR__).'assets/admin-style.css', null, CSS_VERSION  );
 	}
